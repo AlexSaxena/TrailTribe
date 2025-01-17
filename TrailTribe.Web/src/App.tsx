7a3b4ed1842447a0fcc878a8de570e99cca156aa
Alex Saxena <saxenaalex@gmail.com>
2025-01-17 14:04:53 +0100

    Adds & Configures TanStack router

--- TrailTribe.Web/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="text-orange-500 text-center">
-      <h1 className="text-4xl font-bold">Welcome to TrailTribe!</h1>
-    </div>
-  );
-}
-
-export default App;
