1d4c64b23ce42e298c530f69a431b5bd437c2683
Alex Saxena <saxenaalex@gmail.com>
2025-01-17 14:43:54 +0100

    Adds custom color palette for future use

--- TrailTribe.Web/tailwind.config.js
+++ TrailTribe.Web/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FD6F2F", // Vibrant Orange
+        secondary: "#FFEAD1", // Soft Peach
+        accent: "#DCE4B8", // Muted Green
+        dark: "#515739", // Dark Olive
+      },
+    },
   },
   plugins: [],
 };
