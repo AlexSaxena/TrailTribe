aff36e2fe47f8b7bc26353e84c964afe9ab7c515
Alex Saxena <saxenaalex@gmail.com>
2025-01-18 20:54:16 +0100

    Adds GetUser by Id endpoint

--- TrailTribe.Api/Controllers/UserController.cs
+++ TrailTribe.Api/Controllers/UserController.cs
@@ -21,4 +21,19 @@ public class UserController : ControllerBase
     {
        return await _context.Users.ToListAsync();
     }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+        
+        return user;
+    }
+
+
 }
