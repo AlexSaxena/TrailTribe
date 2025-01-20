d2ae756def09d610d7a1fdf2cf5d1356fd5aca7d
Alex <saxenaalex@gmail.com>
2025-01-20 09:35:05 +0100

    removes Id from link

--- TrailTribe.Web/src/components/TrailCard.tsx
+++ TrailTribe.Web/src/components/TrailCard.tsx
@@ -28,8 +28,7 @@ const TrailCard: React.FC<TrailCardProps> = ({
           {description}
         </p>
         <Link
-          to="/trailDetails/$id"
-          params={{ id: id }}
+          to="/trailDetails"
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           View More
